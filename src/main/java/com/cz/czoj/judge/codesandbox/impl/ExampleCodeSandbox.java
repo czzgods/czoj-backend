e3abdf9ef5b7906ca92249a153e7e26b0bf8cb35
cz <3302076969@qq.com>
2024-07-22 20:14:55 +0800

    ADD代理模式对代码沙箱接口进行优化

--- src/main/java/com/cz/czoj/judge/codesandbox/impl/ExampleCodeSandbox.java
+++ src/main/java/com/cz/czoj/judge/codesandbox/impl/ExampleCodeSandbox.java
@@ -3,6 +3,11 @@ package com.cz.czoj.judge.codesandbox.impl;
 import com.cz.czoj.judge.codesandbox.CodeSandbox;
 import com.cz.czoj.judge.codesandbox.model.ExecuteCodeRequest;
 import com.cz.czoj.judge.codesandbox.model.ExecuteCodeResponse;
+import com.cz.czoj.model.dto.questionsubmit.JudgeInfo;
+import com.cz.czoj.model.enums.JudgeInfoMessageEnum;
+import com.cz.czoj.model.enums.QuestionSubmitStatusEnum;
+
+import java.util.List;
 
 /**
  * 示例代码沙箱（仅为了跑通业务流程）
@@ -10,7 +15,17 @@ import com.cz.czoj.judge.codesandbox.model.ExecuteCodeResponse;
 public class ExampleCodeSandbox implements CodeSandbox {
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
-        System.out.println("示例代码沙箱");
-        return null;
+        List<String> inputList = executeCodeRequest.getInputList();
+        ExecuteCodeResponse executeCodeResponse = new ExecuteCodeResponse();
+        executeCodeResponse.setOutputList(inputList);
+        executeCodeResponse.setMessage("测试执行成功");
+        executeCodeResponse.setStatus(QuestionSubmitStatusEnum.SUCCEED.getValue());
+        JudgeInfo judgeInfo = new JudgeInfo();
+        judgeInfo.setMessage(JudgeInfoMessageEnum.ACCEPTED.getText());
+        judgeInfo.setMemory(100L);
+        judgeInfo.setTime(100L);
+        executeCodeResponse.setJudgeInfo(judgeInfo);
+        return executeCodeResponse;
+
     }
 }
--- src/main/java/com/cz/czoj/judge/codesandbox/model/ExecuteCodeResponse.java
+++ src/main/java/com/cz/czoj/judge/codesandbox/model/ExecuteCodeResponse.java
@@ -1,5 +1,6 @@
 package com.cz.czoj.judge.codesandbox.model;
 
+import com.cz.czoj.model.dto.questionsubmit.JudgeInfo;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
